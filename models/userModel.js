fd82bad70369b9ee3dd8fbcfb9985b3b6746efe9
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-02-12 23:00:18 +0600

    userModel add

---
+++ models/userModel.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const UserSchema = mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        firstname: {
+            type: String,
+            required: true,
+        },
+        lastname: {
+            type: String,
+            required: true,
+        },
+        isAdmin: {
+            type: Boolean,
+            default: false,
+        },
+        about: String,
+    },
+    { timestamps: true }
+);
+
+const UserModel = mongoose.model("Users", UserSchema);
+export default UserModel;
